7c6ac3318af13d49d66a68debb4c30dfeff21355
kartikag1 <32024148+kartikag1@users.noreply.github.com>
2019-11-25 18:38:12 +0530

    Create converter.js

---
+++ converter.js
@@ -0,0 +1,30 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+var html1=[];
+var html11=[];
+
+fs.createReadStream('C:/Users/Ritika/Desktop/yah.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    for(var i=0;i<results.length;i++){
+    	html1+="<option value='"+results[i]['OEDV']+"'>"+results[i]['Osage Exploration and Development, Inc.']+"</option>";
+    }
+    fs.writeFile('mynewfile3.txt', html1, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+  });
+  fs.createReadStream('C:/Users/Ritika/Desktop/yah.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    for(var i=0;i<results.length;i++){
+    	html11+="'"+results[i]['OEDV']+"'"+",";
+    }
+    fs.writeFile('mynewfile3333.txt', html11, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+  });
